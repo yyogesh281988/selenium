93ff838a0913b7fb222f98ac7bd940c56e15c54a
yyogesh281988 <30377127+yyogesh281988@users.noreply.github.com>
2018-06-21 13:33:58 +0530

    project setup

---
+++ src/Rest/TestRest.java
@@ -0,0 +1,18 @@
+package Rest;
+
+public class TestRest {
+	
+	TestRest()
+	{
+		System.out.println("Enjoy");
+	}
+	  
+	public void a()
+	{
+		TestRest tr=new TestRest();
+		
+		System.out.println("this is function");
+	}
+	
+
+}
---
+++ src/Rest/test2.java
@@ -0,0 +1,10 @@
+package Rest;
+
+public class test2  {
+	
+	public static void main(String[] args) {
+		
+		TestRest tr=new TestRest();
+	}
+
+}
